80e48de5ad424fec3f01e3775bbd9fa87f633ed5
rbrown29 <53094729+rbrown29@users.noreply.github.com>
2024-03-18 12:21:54 -0700

    added search engine and other updates

--- app.js
+++ app.js
@@ -135,6 +135,33 @@ app.get("/biking", ensureAuthenticated, (req, res) => {
   res.render("bikeShow.ejs", { bikeTrails: bikeTrails.BikingTrails });
 });
 
+app.get("/search", ensureAuthenticated, async (req, res) => {
+  const query = req.query.query; // Get query from URL parameter
+
+  // Check if 'query' is not provided or empty
+  if (!query) {
+    return res.render('search', { results: null, message: "" });
+  }
+
+  const options = {
+    method: 'GET',
+    url: 'https://google-web-search1.p.rapidapi.com/',
+    params: { query: query, limit: '20', related_keywords: 'true' },
+    headers: {
+      'X-RapidAPI-Key': process.env.RAPIDAPI_KEY,
+      'X-RapidAPI-Host': 'google-web-search1.p.rapidapi.com'
+    }
+  };
+
+  try {
+    const response = await axios.request(options);
+    res.render('search', { results: response.data.results, message: null });
+  } catch (error) {
+    console.error(error);
+    res.render('search', { results: null, message: "Failed to fetch results. Please try again." });
+  }
+});
+
 
 
 
