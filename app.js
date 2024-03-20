8aaa7031bc4c7538ddbb803e38628506832e8186
rbrown29 <53094729+rbrown29@users.noreply.github.com>
2024-03-20 11:20:32 -0700

    added weather to camping & biking

--- app.js
+++ app.js
@@ -78,6 +78,7 @@ const trailsController = require("./controllers/trails.js");
 app.use("/trailsData", ensureAuthenticated, trailsController);
 
 const bikingController = require("./controllers/biking.js");
+const BikingTrail = require("./models/biking.js");
 app.use("/bikingData", ensureAuthenticated, bikingController);
 
 
@@ -119,11 +120,11 @@ app.get('/show', ensureAuthenticated, async (req, res) => {
 
       try {
         const weatherResponse = await axios.get(url);
-        trail.weather = weatherResponse.data; // trail data with weather information
+        trail.weather = weatherResponse.data; 
         trail.weather.temperatureF = (trail.weather.main.temp - 273.15) * 9/5 + 32;
       } catch (error) {
         console.error('Failed to fetch weather data', error);
-        trail.weather = null; // Handle errors 
+        trail.weather = null;
       }
     }
     return trail;
@@ -132,12 +133,48 @@ app.get('/show', ensureAuthenticated, async (req, res) => {
   res.render('show.ejs', { trails: trailsWithWeather, json: json.items }); // Render with enhanced data
 });
 
-app.get("/biking", ensureAuthenticated, (req, res) => {
-  res.render("bikeShow.ejs", { bikeTrails: bikeTrails.BikingTrails });
+app.get("/biking", ensureAuthenticated, async (req, res) => {
+  let trailsWithWeather = await Promise.all(bikeTrails.BikingTrails.map(async (BikingTrail) => {
+    if (BikingTrail.lat && BikingTrail.lon) {
+      const latitude = BikingTrail.lat;
+      const longitude = BikingTrail.lon;
+      const apiKey = process.env.OPENWEATHER_API_KEY; 
+      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+  
+      try {
+        const weatherResponse = await axios.get(url);
+        const tempKelvin = weatherResponse.data.main.temp;
+        BikingTrail.weather = weatherResponse.data;
+        BikingTrail.weather.temperatureF = (tempKelvin - 273.15) * 9/5 + 32;
+      } catch (error) {
+        console.error('Failed to fetch weather data', error);
+        BikingTrail.weather = null;  
+      }
+    }
+    return BikingTrail;
+  }));
+  res.render("bikeShow.ejs", {BikeTrails: trailsWithWeather, bikeTrails: bikeTrails.BikingTrails });
 });
 
-app.get("/camping", ensureAuthenticated,  (req, res) => {
-  res.render("camp.ejs", { camping: camping.camp });
+app.get("/camping", ensureAuthenticated, async  (req, res) => {
+  let trailsWithWeather = await Promise.all(camping.camp.map(async (camp) => {
+    if (camp.location) {
+      const { latitude, longitude } = camp.location;
+      const apiKey = process.env.OPENWEATHER_API_KEY; 
+      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${process.env.OPENWEATHER_API_KEY}`;
+
+      try {
+        const weatherResponse = await axios.get(url);
+        camp.weather = weatherResponse.data; 
+        camp.weather.temperatureF = (camp.weather.main.temp - 273.15) * 9/5 + 32;
+      } catch (error) {
+        console.error('Failed to fetch weather data', error);
+        camp.weather = null; 
+      }
+    }
+    return camp;
+  }));
+  res.render("camp.ejs", {camping: trailsWithWeather, camping: camping.camp });
 });
 
 app.get("/search", ensureAuthenticated, async (req, res) => {
